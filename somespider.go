aa3606dd52d2d015fd1d7187d4462c190bae9dbf
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-11 18:01:52 -0700

    Initial

---
+++ somespider.go
@@ -0,0 +1,27 @@
+package somespider
+
+import (
+	"log/slog"
+	"os"
+	"path/filepath"
+
+	"github.com/adrg/xdg"
+)
+
+func GenPath(configRelPath string) (string, error) {
+	configFilePath, err := xdg.ConfigFile(configRelPath)
+	if err != nil {
+		return "", err
+	}
+
+	dirPerm := os.FileMode(0o700)
+
+	d := filepath.Dir(configFilePath)
+
+	if err := os.MkdirAll(d, dirPerm); err != nil {
+		slog.Error("cache", "mkdir", "error", err.Error())
+		return "", err
+	}
+
+	return configFilePath, nil
+}
